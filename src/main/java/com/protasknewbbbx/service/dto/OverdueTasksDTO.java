f28e2e3eb48b7e8a4dc8985ff0e52b66128304e9
Ramzi CHATTI <ramzychatti90@gmail.com>
2026-04-08 19:33:50 +0200

    [FORGE_DEV] YC-PROTAS-S1-003: src/main/java/com/protasknewbbbx/service/dto/OverdueTasksDTO.java

---
+++ src/main/java/com/protasknewbbbx/service/dto/OverdueTasksDTO.java
@@ -0,0 +1,45 @@
+package com.protasknewbbbx.service.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class OverdueTasksDTO implements Serializable {
+
+    private Long overdueCount;
+
+    public OverdueTasksDTO() {
+        // Empty constructor needed for Jackson.
+    }
+
+    public OverdueTasksDTO(Long overdueCount) {
+        this.overdueCount = overdueCount;
+    }
+
+    public Long getOverdueCount() {
+        return overdueCount;
+    }
+
+    public void setOverdueCount(Long overdueCount) {
+        this.overdueCount = overdueCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OverdueTasksDTO that = (OverdueTasksDTO) o;
+        return Objects.equals(overdueCount, that.overdueCount);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(overdueCount);
+    }
+
+    @Override
+    public String toString() {
+        return "OverdueTasksDTO{" +
+               "overdueCount=" + overdueCount +
+               '}';
+    }
+}
